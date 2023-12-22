ea2c876f41bae37826100b6a4618a7bb6d4650fb
premnarayanp <premnarayanpatel223@gmail.com>
2023-12-22 15:42:49 +0530

    change mongodb local URL to online URL ,process.env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect(process.env.MONGO_DB_URL);
-mongoose.connect("mongodb://127.0.0.1/quiz_api");
+mongoose.connect(process.env.MONGO_DB_URL);
+//mongoose.connect("mongodb://127.0.0.1/quiz_api");
 
 const db = mongoose.connection;
 
